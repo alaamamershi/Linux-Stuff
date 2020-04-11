ff3eb2495f194c6ecff8bbb49c03a608e1c9433f
Alaam Amershi <alaamamershi@gmail.com>
2020-04-11 16:33:58 -0600

    Update DebianSetup.sh

--- Setup Scripts/DebianSetup.sh
+++ Setup Scripts/DebianSetup.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
 #########################################################################################
-# DebianGnomeSetup.sh
+# DebianSetup.sh
 # 
 # Author:			Alaam Amershi
 # Date:				10/26/19
-# Last Modified:	04/10/20
+# Last Modified:	04/11/20
 # 
 # Description:
 # Setup script for fresh Debian installs.
